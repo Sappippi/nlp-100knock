31c647b7567d76d8f0d55a16841c7e48b62fb43a
Sappippi <nopu1783@gmail.com>
2024-11-12 12:50:04 +0900

    write chapter3

--- src/my_functions.py
+++ src/my_functions.py
@@ -1,4 +1,5 @@
 import random as rd
+import re
 
 
 def n_gram(sentence: str | list | tuple, 
@@ -42,3 +43,28 @@ def str_shuff(sentence):
     """
     length = len(sentence)
     return "".join(rd.sample(sentence, length))
+
+def extract_category_name(string: str):
+    """カテゴリを含む行からカテゴリ名だけを抽出
+    """
+    category_name = re.sub('.+:', '', string) # カテゴリ名より前の文字列を削除
+    category_name = re.sub('(\|.)?\]+(\\n)?','',category_name)
+    return category_name
+
+def print_secname_level(string: str):
+    """セクション名とそのレベルを表示する
+    """
+    secname = string.strip('=')
+    total_eqs = len(string.replace(secname,"")) # strに含まれる'='の総数
+    level = total_eqs // 2 -1
+    print("セクション名: ", secname, "\nレベル: ", level)
+
+def remove_internal_link(string: str):
+    """内部リンクを取り除く
+    """
+    internal_links = re.findall('\[\[[^\]]+\]\]',string)
+    list_article_name = [re.sub('(\|[^\|^\[]+)+]]', '', article_name).lstrip("[").rstrip("]") for article_name in internal_links]
+
+    for article in list_article_name:
+        string = re.sub('\[\[[^\]]+\]\]', article, string)
+    return string
