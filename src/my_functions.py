ec3620ee7f03c77e1c24756ca73cf8f01235737c
Sappippi <nopu1783@gmail.com>
2024-11-14 21:13:09 +0900

    chapter4-1

--- src/my_functions.py
+++ src/my_functions.py
@@ -1,5 +1,7 @@
 import random as rd
 import re
+import pandas as pd
+import itertools
 
 
 def n_gram(sentence: str | list | tuple, 
@@ -44,27 +46,45 @@ def str_shuff(sentence):
     length = len(sentence)
     return "".join(rd.sample(sentence, length))
 
-def extract_category_name(string: str):
-    """カテゴリを含む行からカテゴリ名だけを抽出
-    """
-    category_name = re.sub('.+:', '', string) # カテゴリ名より前の文字列を削除
-    category_name = re.sub('(\|.)?\]+(\\n)?','',category_name)
-    return category_name
+# def extract_category_name(string: str):
+#     """カテゴリを含む行からカテゴリ名だけを抽出
+#     """
+#     category_name = re.sub('.+:', '', string) # カテゴリ名より前の文字列を削除
+#     category_name = re.sub('(\|.)?\]+(\\n)?','',category_name)
+#     return category_name
 
-def print_secname_level(string: str):
-    """セクション名とそのレベルを表示する
-    """
-    secname = string.strip('=')
-    total_eqs = len(string.replace(secname,"")) # strに含まれる'='の総数
-    level = total_eqs // 2 -1
-    print("セクション名: ", secname, "\nレベル: ", level)
+# def print_secname_level(string: str):
+#     """セクション名とそのレベルを表示する
+#     """
+#     secname = string.strip('=')
+#     total_eqs = len(string.replace(secname,"")) # strに含まれる'='の総数
+#     level = total_eqs // 2 -1
+#     print("セクション名: ", secname, "\nレベル: ", level)
 
-def remove_internal_link(string: str):
-    """内部リンクを取り除く
-    """
-    internal_links = re.findall('\[\[[^\]]+\]\]',string)
-    list_article_name = [re.sub('(\|[^\|^\[]+)+]]', '', article_name).lstrip("[").rstrip("]") for article_name in internal_links]
+# def remove_internal_link(string: str):
+#     """内部リンクを取り除く
+#     """
+#     internal_links = re.findall('\[\[[^\]]+\]\]',string)
+#     list_article_name = [re.sub('(\|[^\|^\[]+)+]]', '', article_name).lstrip("[").rstrip("]") for article_name in internal_links]
 
-    for article in list_article_name:
-        string = re.sub('\[\[[^\]]+\]\]', article, string)
-    return string
+#     for article in list_article_name:
+#         string = re.sub('\[\[[^\]]+\]\]', article, string)
+#     return string
+
+def divide_conquer_split(df: pd.DataFrame, list_idx_eos):
+    """形態素のデータフレームを文単位に分割する
+    """
+    length = len(list_idx_eos)
+    # 基底
+    if length == 1:
+        key_names = df.columns
+        sentence = [dict(zip(key_names, df.iloc[i].to_list())) for i in range(df.shape[0])]
+        return [sentence]
+
+    # 分割統治
+    middle = length // 2
+    left_end = list_idx_eos[middle] + 1
+    list_left = divide_conquer_split(df.iloc[:left_end], list_idx_eos[:middle])
+    temp = list(map(lambda x: x-list_idx_eos[middle]-1, list_idx_eos[middle:])) # 右側はeosの位置がズレるので修正
+    list_right = divide_conquer_split(df.iloc[left_end:], temp)
+    return list_left + list_right
